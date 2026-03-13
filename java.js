c7ce66e8fb52158ecd8d27c0d4785d6141f0c5bd
milhuisenmark748-source <milhuisenmark748@gmail.com>
2026-03-13 15:17:55 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1188,24 +1188,34 @@ if (currentCar.history) {
 }
 
 window.onload = function() {
-    const carID = localStorage.getItem('carID') || "1";
+    const carID = localStorage.getItem('carID');
     const currentCar = carData.find(car => car.id == carID);
     const select = document.getElementById('edition-select');
+    const mainImg = document.getElementById("MainImg");
 
-    if (currentCar && select) {
-        select.innerHTML = "";
-        const availableEditions = Object.keys(currentCar.editions || {"standard": {}});
-        availableEditions.forEach(editionKey => {
-            const option = document.createElement("option");
-            option.value = editionKey;
-            option.text = editionKey.charAt(0).toUpperCase() + editionKey.slice(1);
-            select.appendChild(option);
-        });
-        select.value = availableEditions[0];
-        updatePrices(select);
+    if (currentCar) {
+        if (mainImg) {
+            mainImg.src = currentCar.image;
+        }
+
+        if (select) {
+            select.innerHTML = "";
+            const availableEditions = Object.keys(currentCar.editions || {"standard": {}});
+            availableEditions.forEach(editionKey => {
+                const option = document.createElement("option");
+                option.value = editionKey;
+                option.text = editionKey.charAt(0).toUpperCase() + editionKey.slice(1);
+                select.appendChild(option);
+            });
+            select.value = availableEditions[0];
+            updatePrices(select);
+        }
+
+        document.querySelector(".single-pro-details h4").innerText = currentCar.brand;
+        document.querySelector(".single-pro-details h2").innerText = currentCar.name;
+        document.querySelector(".single-pro-details span").innerText = currentCar.description;
     }
 };
-
 // Function to add a car to the cart
 function addToCart(buttonElement) {
     // 1. Get data from the specific button clicked
