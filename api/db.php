929b5d989d6e6291133c32e75167685142560ff8
milhuisenmark748-source <milhuisenmark748@gmail.com>
2026-03-15 20:25:30 +0530

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,5 +1,5 @@
 <?php
-// We use the names of the variables you set in Vercel
+// We use the KEY NAMES you typed into Vercel's dashboard
 $host = getenv('TIDB_HOST');
 $user = getenv('TIDB_USER');
 $pass = getenv('TIDB_PASSWORD');
@@ -7,8 +7,6 @@ $db_name = getenv('TIDB_DATABASE');
 $port = getenv('TIDB_PORT') ?: 4000;
 
 $conn = mysqli_init();
-
-// Required for TiDB Cloud secure connection
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
 if (!mysqli_real_connect($conn, $host, $user, $pass, $db_name, $port)) {
