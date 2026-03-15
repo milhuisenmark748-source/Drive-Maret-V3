88a3e52c7af2090be5703d3ad4a624419c9837dc
milhuisenmark748-source <milhuisenmark748@gmail.com>
2026-03-15 12:15:25 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,17 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Registration Successful!'); window.location.href='login.html';</script>";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
