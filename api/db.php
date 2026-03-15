e00afe99cb1fa4d34ae1314e2f643ff25f99177b
milhuisenmark748-source <milhuisenmark748@gmail.com>
2026-03-15 21:46:31 +0530

    s

--- api/db.php
+++ api/db.php
@@ -1,5 +1,4 @@
 <?php
-// We use the KEY NAMES you typed into Vercel's dashboard
 $host = getenv('TIDB_HOST');
 $user = getenv('TIDB_USER');
 $pass = getenv('TIDB_PASSWORD');
@@ -7,9 +6,12 @@ $db_name = getenv('TIDB_DATABASE');
 $port = getenv('TIDB_PORT') ?: 4000;
 
 $conn = mysqli_init();
+
+// This line prepares the SSL connection
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-if (!mysqli_real_connect($conn, $host, $user, $pass, $db_name, $port)) {
+// Added MYSQLI_CLIENT_SSL at the end to force secure transport
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db_name, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
